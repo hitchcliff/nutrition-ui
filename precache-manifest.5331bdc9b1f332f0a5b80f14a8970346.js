e533bfc65c1cc0c5138c14596dcb7893ba5aa978
Kevin Nacario <hitchcliff1@gmail.com>
2020-08-04 22:30:03 +0800

    Updates

---
+++ precache-manifest.5331bdc9b1f332f0a5b80f14a8970346.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ca2079ff6199e0f300e13cef75e35c",
+    "url": "/nutrition-ui/index.html"
+  },
+  {
+    "revision": "04f3e990b3eddc99a5a1",
+    "url": "/nutrition-ui/static/css/main.96269ef1.chunk.css"
+  },
+  {
+    "revision": "0ed79dfc3911e4f8b7c3",
+    "url": "/nutrition-ui/static/js/2.9b27a9f7.chunk.js"
+  },
+  {
+    "revision": "2206f6f6cbbf99cae08d01b2952f71ad",
+    "url": "/nutrition-ui/static/js/2.9b27a9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f3e990b3eddc99a5a1",
+    "url": "/nutrition-ui/static/js/main.588949d3.chunk.js"
+  },
+  {
+    "revision": "ee24dfb6cd66e0fa5679",
+    "url": "/nutrition-ui/static/js/runtime-main.8031d2cc.js"
+  },
+  {
+    "revision": "5e2c838463549624c94127bbcf107082",
+    "url": "/nutrition-ui/static/media/food.5e2c8384.png"
+  },
+  {
+    "revision": "07e058f561c274df96c3d7fd439253e1",
+    "url": "/nutrition-ui/static/media/leaves1.07e058f5.png"
+  },
+  {
+    "revision": "09728f61a2b8c1ccb34bab9749a3bd1e",
+    "url": "/nutrition-ui/static/media/leaves2.09728f61.png"
+  },
+  {
+    "revision": "c3d248abc2aee95251f6d81c8ffe7bb9",
+    "url": "/nutrition-ui/static/media/leaves3.c3d248ab.png"
+  },
+  {
+    "revision": "74e2dc47e1ae744b47c6900c1fc8c259",
+    "url": "/nutrition-ui/static/media/leaves4.74e2dc47.png"
+  },
+  {
+    "revision": "93e725820701e99f8d72a17733974f7d",
+    "url": "/nutrition-ui/static/media/logo.93e72582.svg"
+  },
+  {
+    "revision": "4c4d13cc8b9bb4a944aaf9dc182ab7cd",
+    "url": "/nutrition-ui/static/media/message.4c4d13cc.svg"
+  },
+  {
+    "revision": "f5a9ec8ec528d9d5a9935b601f47f72c",
+    "url": "/nutrition-ui/static/media/play.f5a9ec8e.svg"
+  },
+  {
+    "revision": "c81f7174d75f7eb10d4b74a0c3dc2da6",
+    "url": "/nutrition-ui/static/media/truck.c81f7174.svg"
+  }
+]);
